2d2f58fae07ce68602bcd5075ebac12277fd856d
lorenzopablo91 <lorenzopablo91@gmail.com>
2026-02-18 08:57:37 -0300

    updated environments

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: 'https://financial-tracker-k4px.onrender.com/api'
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL: 'https://financial-tracker-k4px.onrender.com/api',
-  // API_URL: 'http://localhost:3000/api',
+  // API_URL: 'https://financial-tracker-k4px.onrender.com/api',
+  API_URL: 'http://localhost:3000/api',
   production: false,
 };
